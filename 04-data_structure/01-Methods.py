2c004f7902a5b755d4e22754270c77a23ad78b13
Your Name <wjdgml3834@naver.com>
2022-09-15 09:48:46 +0900

    💻 Methods

---
+++ 04-data_structure/01-Methods.py
@@ -0,0 +1,13 @@
+# 파이썬에서 배워야 할 데이터 구조는 3가지가 있다.
+# List, Tuples, Dicts
+
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+# 메서드: 데이터 뒤에 연결된 함수. 내부에 다양한 function을 가지고 있다.
+
+name = "JungHee"
+print(name.upper())  # JUNGHEE
+print(name.capitalize())  # Junghee
+print(name.startswith("J"))  # True
+print(name.replace("J", "😃"))  # 😃ungHee
+print(name.endswith("a"))  # False
